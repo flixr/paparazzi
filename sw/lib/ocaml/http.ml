f71e429db0dae66f5ee0e4330ecf60cbcf6b9452
Felix Ruess <felix.ruess@gmail.com>
2015-03-16 18:41:28 +0100

    [ocaml] use cppo to check netclient version
    Use the simple cppo preprocessor for OCaml to use Nethttp_client instead of Http_client for ocamlnet versions >= 4.0.2
    See issue #1133
    
    Note: this requires that cppo is installed.
    On Debian/Ubuntu:
    sudo apt-get install cppo
    via OPAM:
    opam install cppo

--- sw/lib/ocaml/http.ml
+++ sw/lib/ocaml/http.ml
@@ -2,7 +2,12 @@ exception Failure of string
 exception Not_Found of string
 exception Blocked of string
 
-open Http_client
+module H =
+#if NETCLIENT_VERSION >= (4, 0, 2)
+ Nethttp_client
+#else
+ Http_client
+#endif
 
 let file_of_url = fun ?dest url ->
   if String.sub url 0 7 = "file://" then
@@ -12,9 +17,9 @@ let file_of_url = fun ?dest url ->
       match dest with
           Some s -> s
         | None -> Filename.temp_file "fp" ".wget" in
-    let call = new Http_client.get url in
+    let call = new H.get url in
     call#set_response_body_storage (`File (fun () -> tmp_file));
-    let pipeline = new Http_client.pipeline in
+    let pipeline = new H.pipeline in
     pipeline#set_proxy_from_environment ();
     pipeline#add call;
     pipeline#run ();
